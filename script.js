9ef40dc1b914daa2c28e8003eeabf83552f9c62c
MOHAMMAD RAZA <razamohammad0589@gmail.com>
2025-03-08 23:54:03 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var tablinks = document.querySelectorAll('.tab-links');
+let tabcontent = document.querySelectorAll('.tab-content');
+
+function opentab(event, tabname) {
+    for (let tabclick of tablinks) {
+        tabclick.classList.remove('active-link');
+    }
+    for (let tabItem of tabcontent) {
+        tabItem.classList.remove('active-tab');
+    }
+
+    event.currentTarget.classList.add('active-link');
+
+    const targetElement = document.getElementById(tabname);
+    if (!targetElement) {
+        console.error(`Element with id "${tabname}" not found.`);
+        return;
+    }
+    targetElement.classList.add('active-tab');
+}
+
+
+
+
+// ***************** Download Resume************
+
+
+
+function clickBtn() {
+    let comfirmed = confirm('do you want to open resume');
+    if (comfirmed) {
+        window.location = "seraj-resume.pdf"
+    }
+    else {
+        return
+    }
+}
+
+
+
+// **************** menu close and open ***********
+
+let sideMenu = document.getElementById('sidemenu')
+
+function closemenu() {
+    sideMenu.style.width = "0px"
+}
+function openmenu() {
+    sideMenu.style.width = "200px"
+}
